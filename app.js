990f84150bb58e88d19d2899eebf26ea01baf7d4
mereth <mereth78@gmail.com>
2014-08-21 15:24:57 +0200

    move bin/www content to app.js

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
+var debug = require('debug')('pamm-server');
 var express = require('express');
+var mongodb = require('mongodb').MongoClient;
+
 var path = require('path');
 var favicon = require('static-favicon');
 var logger = require('morgan');
@@ -12,51 +15,60 @@ var usage = require('./routes/usage');
 var app = express();
 
 var devmode = (app.get('env') === 'development');
+var serverip = (process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+var serverport = (process.env.OPENSHIFT_NODEJS_PORT || 8080);
+var mongodburl = (process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost/') + 'pamm';
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.enable('trust proxy');
-
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(require('stylus').middleware(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Make the database accessible to our router
-app.use(function(req,res,next){
-    req.database = app.get('database');
-    next();
-});
-
-app.use('/', routes);
-app.use('/users', users);
-app.use('/api/usage', usage);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-    var data = {
-        message: err.message,
-        error: devmode ? err : {}
-    }
+mongodb.connect(mongodburl, function(err, database) {
+    if(err) throw err;
     
-    res.status(err.status || 500);
-    if(req.path.indexOf('/api/') === 0)
-        res.send(data);
-    else
-        res.render('error', data);
+    // view engine setup
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'jade');
+    
+    app.enable('trust proxy');
+    
+    app.use(favicon());
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded());
+    app.use(cookieParser());
+    app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+    app.use(express.static(path.join(__dirname, 'public')));
+    
+    // Make the database accessible to our router
+    app.use(function(req,res,next){
+        req.database = database;
+        next();
+    });
+    
+    app.use('/', routes);
+    app.use('/users', users);
+    app.use('/api/usage', usage);
+    
+    // catch 404 and forward to error handler
+    app.use(function(req, res, next) {
+        var err = new Error('Not Found');
+        err.status = 404;
+        next(err);
+    });
+    
+    // error handler
+    app.use(function(err, req, res, next) {
+        var data = {
+            message: err.message,
+            error: devmode ? err : {}
+        }
+        
+        res.status(err.status || 500);
+        if(req.path.indexOf('/api/') === 0)
+            res.send(data);
+        else
+            res.render('error', data);
+    });
+
+    // start the server
+    var server = app.listen(serverport, serverip, function() {
+        debug('Express server listening on ' + server.address().address + ':' + server.address().port);
+    });
 });
-
-
-module.exports = app;
