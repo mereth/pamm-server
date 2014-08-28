f8d4771fd8832bef8478aac17de1fa438563e9bb
mereth <mereth78@gmail.com>
2014-08-28 21:06:40 +0200

    upgrade morgan logger and activate "combined" logs

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ var start = function() {
     app.enable('trust proxy');
     
     app.use(favicon());
-    app.use(logger('dev'));
+    app.use(logger('combined'));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded());
     app.use(cookieParser());
