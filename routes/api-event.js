4707b5f4915d2b2de0e8d04d22b3e9fff9c14b49
mereth <mereth78@gmail.com>
2014-09-14 15:10:34 +0200

    invert events order and limit to the last 10

--- routes/api-event.js
+++ routes/api-event.js
@@ -7,7 +7,7 @@ var db;
 /* GET events */
 router.get('/', function(req, res, next) {
     db = req.database;
-    db.collection('events').find().toArray(function(err, events) {
+    db.collection('events').find({},{sort: {$natural: -1}, limit: 10}).toArray(function(err, events) {
         if(err) return next(err);
         
         _.forEach(events, function(event) {
